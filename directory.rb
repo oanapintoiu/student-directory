46867e6ec40be3a4e46241deb9936e3ab1164d6f
Oana Pintoiu <oana.pintoiu@yahoo.com>
2023-03-07 23:57:37 +0000

    List of Students-step 7

--- directory.rb
+++ directory.rb
@@ -1,39 +1,52 @@
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon",
-"Norman Bates"
+{name: "Dr. Hannibal Lecter", cohort: :november},
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone", cohort: :november},
+{name: "Alex DeLarge", cohort: :november},
+{name: "The Wicked Witch of the West", cohort: :november},
+{name: "Terminator", cohort: :november},
+{name: "Freddy Krueger", cohort: :november},
+{name: "The Joker", cohort: :november},
+{name: "Joffrey Baratheon", cohort: :november},
+{name: "Norman Bates", cohort: :november},
 ]
 
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    # create an empty array
+    students = []
+    # get the first name
+    name = gets.chomp
+    # while the name is not empty, repeat this code
+    while !name.empty? do
+      # add the student hash to the array
+      students << {name: name, cohort: :november}
+      puts "Now we have #{students.count} students"
+      # get another name from the user
+      name = gets.chomp
+    end
+    # return the array of students
+    students
+  end
+
 def print_header
-puts "The students of Villain Academy"
-puts "----------------------"
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
-def print names
-
-
-names.each do |name|
-    puts name
+def print(students)
+students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
 end
 end
 
-def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+    puts "Overall, we have #{students.count} great students."
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
-# the value of the first student will be assigned to student
-# then it will execute the block and puts the name of the studen 
-# on the screen. then ruby will perform a second iteration
-# assigning the next name/value to students and so on until the end
-# of the array.
