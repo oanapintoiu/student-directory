786e87d29d490ffe7a47dcb5888b13f6f5cb70e7
Oana Pintoiu <oana.pintoiu@yahoo.com>
2023-03-08 18:56:34 +0000

    Step8 Ex1 dirrectory.rb

--- directory.rb
+++ directory.rb
@@ -37,8 +37,9 @@ def print_header
 end
 
 def print(students)
-students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+students.each_with_index do |student, index|
+  indexplusone = index + 1
+    puts "#{indexplusone}.#{student[:name]} (#{student[:cohort]} cohort)"
 end
 end
 
