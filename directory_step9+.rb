f3ce058809e8706a2711eb26c12ad84ea1beafdc
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-03-12 14:57:32 +0000

    directory used for Step 9 onwards

--- directory_step9+.rb
+++ directory_step9+.rb
@@ -46,7 +46,35 @@ def print_footer(students)
     puts "Overall, we have #{students.count} great students."
 end
 
-students = input_students
-print_header
+def interactive_menu
+  students = []
+  loop do
+# 1. print the menu and ask the user what to do
+puts "1. Input the students"
+puts "2. Show the students"
+puts "9. Exit"
+# read the input and put it into a variable
+selection = gets.chomp
+# do what the user has asked
+case selection
+when "1"
+  # input students
+  students = input_students
+when "2"
+  # show the students
+ print_header
 print(students)
 print_footer(students)
+when "9"
+  # cause the program to terminate
+  exit 
+else
+  puts "I don't know what you meant, try again."
+end
+# 2. read the input and save it into a variable
+# 3. do what the user has asked
+# 4. repeat from step 1 
+  end
+end 
+
+interactive_menu
