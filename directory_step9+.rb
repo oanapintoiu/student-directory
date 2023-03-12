11223501613546d1ea5dd986981be8dafc1eac52
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-03-12 15:49:03 +0000

    directory used for Step 9 onwards

--- directory_step9+.rb
+++ directory_step9+.rb
@@ -38,10 +38,25 @@ def interactive_menu
   end
 end
 
+def save_students
+  # open the file for saving
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    cvs_line = student_data.join(",")
+    file.puts cvs_line
+  end
+  file.close
+end
+
+
+
 def print_menu
   # 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
   # read the input and put it into a variable
 end
@@ -59,6 +74,8 @@ def process(selection)
    input_students # input students
   when "2"
    show_students # show the students
+  when "3"
+    save_students
   when "9"
     exit # cause the program to terminate
   else
