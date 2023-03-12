88c91cafd9b0209fc2dac458c39d4c142133e944
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-03-12 15:38:06 +0000

    directory used for Step 9 onwards

--- directory_step9+.rb
+++ directory_step9+.rb
@@ -1,80 +1,84 @@
 students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november},
+  { name: "Dr. Hannibal Lecter", cohort: :november },
+  { name: "Darth Vader", cohort: :november },
+  { name: "Nurse Ratched", cohort: :november },
+  { name: "Michael Corleone", cohort: :november },
+  { name: "Alex DeLarge", cohort: :november },
+  { name: "The Wicked Witch of the West", cohort: :november },
+  { name: "Terminator", cohort: :november },
+  { name: "Freddy Krueger", cohort: :november },
+  { name: "The Joker", cohort: :november },
+  { name: "Joffrey Baratheon", cohort: :november },
+  { name: "Norman Bates", cohort: :november },
 ]
 
+@students = []
+# an empty array access to all methods
+# using a @ makes variables global
+
 def input_students
-    puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
-    # create an empty array
-    students = []
-    # get the first name
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << { name: name, cohort: :november }
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
     name = gets.chomp
-    # while the name is not empty, repeat this code
-    while !name.empty? do
-      # add the student hash to the array
-      students << {name: name, cohort: :november}
-      puts "Now we have #{students.count} students"
-      # get another name from the user
-      name = gets.chomp
-    end
-    # return the array of students
-    students
   end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+  # read the input and put it into a variable
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  # do what the user has asked
+  case selection
+  when "1"
+   input_students # input students
+  when "2"
+   show_students # show the students
+  when "9"
+    exit # cause the program to terminate
+  else
+    puts "I don't know what you meant, try again."
+  end
+end
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
-end
-end
-
-def print_footer(students)
-    puts "Overall, we have #{students.count} great students."
-end
-
-def interactive_menu
-  students = []
-  loop do
-# 1. print the menu and ask the user what to do
-puts "1. Input the students"
-puts "2. Show the students"
-puts "9. Exit"
-# read the input and put it into a variable
-selection = gets.chomp
-# do what the user has asked
-case selection
-when "1"
-  # input students
-  students = input_students
-when "2"
-  # show the students
- print_header
-print(students)
-print_footer(students)
-when "9"
-  # cause the program to terminate
-  exit 
-else
-  puts "I don't know what you meant, try again."
-end
-# 2. read the input and save it into a variable
-# 3. do what the user has asked
-# 4. repeat from step 1 
   end
-end 
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students."
+end
 
 interactive_menu
